517e66c4207d20bbc78200d57e404cbbcd7da4be
Shad-islam <m.shadislam01@gmail.com>
2025-04-12 21:00:03 +0600

    First class data type and type conversion

---
+++ js/script.js
@@ -0,0 +1,73 @@
+// primitive data types//
+// string, number, boolean, null, undefined, bigint
+let name= "Alice"; 
+let age = 30;
+let isStudent = true;
+let phoneNumber = null;
+let bigNumber = 1234567890123456789012345678901234567890n;
+let address = undefined
+
+
+// let name2  // variable declaration
+// console.log(name2); 
+// name2 = "Bob"; // variable assignment
+// console.log(name2); 
+
+// console.log("name "+ typeof name); // string
+// console.log("age " + typeof age); // number
+// console.log("isStudent " + typeof isStudent); // boolean
+// console.log("phoneNumber " + typeof phoneNumber); // object
+// console.log("bigNumber " + typeof bigNumber); // bigint
+// console.log("address " + typeof address); // undefined
+
+// Type conversion 
+// string converstion //
+// let value = 42;
+// console.log ("Value is a " + typeof value); 
+
+// value = String(value);
+// console.log ("Value is a " + typeof value);
+
+// number conversion //
+// let value2 = "42"; ;
+// console.log(typeof value2);
+
+// value2 = Number(value2)
+// console.log( typeof value2)
+
+// value2 = undefined
+// value2 = Number(value2)
+// console.log(value2);
+
+// value2 = null
+// value2 = Number(value2)
+// console.log(value2);
+
+// value2 = false
+// value2 = Number(value2)
+// console.log(value2);
+
+// value2 = " "
+// value2 = Number(value2)
+// console.log(value2);
+
+
+// value2 = "42px"
+// value2 = Number(value2)
+// console.log(value2);
+
+
+// boolean conversion //
+
+// let value3 = " "
+// value3 = Boolean(value3)
+// console.log(value3)
+
+// value3 = 1
+// value3 = Boolean(value3)
+// console.log(value3)
+
+
+
+
+
