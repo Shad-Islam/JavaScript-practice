c2a894dac5e334675a8d69ea687e9538faca108f
Shad-islam <m.shadislam01@gmail.com>
2025-04-16 21:38:54 +0600

    class 2

---
+++ js/if_else.js
@@ -0,0 +1,22 @@
+// let year = prompt("year?");
+
+// if (year == 2025) {
+//   alert("right");
+// }
+// if (year == 2015) {
+//   alert(year);
+// } else {
+//   alert("wrong");
+// }
+
+// short
+// year == 2025 ? alert("right") : year == 2015 ? alert(year) : alert("Wrong");
+
+// using or and
+// (year == 2025 && alert('right'))||(year == 2015)&&alert(year)||alert('wrong')
+
+// if (year == 2025 || year == 2015) {
+//   alert(year + "right");
+// } else {
+//     alert("wrong");
+// }
---
+++ js/logicalOperators.js
@@ -0,0 +1,48 @@
+// console.log(true || false)
+// console.log( 0 || 1 || false || 100)
+// let x = true
+// let y = false 
+// console.log(x && y)
+
+
+// let hour = 12 
+// let minute = 30 
+// if (hour == 12 && minute == 31){
+//      console.log("The time is 12:30")
+// }else{
+//      console.log("value is falsy ")
+// }
+
+
+// let result = hour && minute && y
+// console.log(typeof result)
+
+
+// result = "" || 0 || y
+// console.log(result)
+
+// console.log(undefined || null || 0 )
+
+
+// let x = true 
+// x = !x 
+// console.log(x)
+
+// let value
+// let result = value
+// console.log(!result)
+
+// console.log(!null)
+
+// console.log(!!"non-empty")
+// console.log(!!null)
+// console.log(Boolean("non-empty"))
+// console.log(Boolean(null))
+
+
+
+
+
+
+
+
