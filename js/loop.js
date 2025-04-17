9f126db7d4fa5f1bbadf572d06831594f26de373
Shad-islam <m.shadislam01@gmail.com>
2025-04-17 20:20:42 +0600

    class 3

---
+++ js/loop.js
@@ -0,0 +1,40 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// for loop //
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
+
+// i = i + 1
+// i++
+
+// skipping part //
+
+// let j = 1;
+// for (j; j <= 10; j++) {
+//   console.log(j);
+// }
+
+
+// let k = 1
+// for(;k<=10;k++){
+//     console.log(k)
+// }
+
+
+// let l = 1
+// for(;l<=10;){
+//     console.log(l++)
+// }
---
+++ js/switchStatement.js
@@ -0,0 +1,16 @@
+let fruits = "Oranges";
+
+switch (fruits) {
+  case "Apple":
+    console.log("Apples are $1.00 a pound");
+    break;
+  case "Mangoes":
+    console.log("Mangoes are $0.58 a pound");
+    break;
+  case "Banana":
+    console.log("Banana are $0.58 a pound");
+    break;
+  default:
+    console.log(`Sorry we are out of ${fruits} `);
+}
+
