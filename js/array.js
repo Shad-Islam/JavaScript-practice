1adfd420cfc67fab4f288d566732562364b88d0f
Shad-islam <m.shadislam01@gmail.com>
2025-04-21 20:30:32 +0600

    class 4

---
+++ js/array.js
@@ -0,0 +1,36 @@
+// let topic = ["HTML","CSS","JS","React","DSA","Tailwind"]
+
+// console.log(topic[3])
+
+// length
+// let numberOfElements = topic.length
+// console.log(numberOfElements)
+
+// let library = topic[3]
+// console.log(library)
+
+// push - add at last
+// topic.push("Python")
+// console.log(topic)
+
+// pop - remove from last
+// topic.pop()
+// console.log(topic)
+
+// shift - remove from first
+// topic.shift()
+// console.log(topic)
+
+// ushift - Add element to the beginning
+// topic.unshift("Fundamental of Computer")
+// console.log(topic)
+
+// multidimensional array
+
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// console.log(arr[1][2])
+
---
+++ js/objects.js
@@ -0,0 +1,39 @@
+// let name = "John Doe"
+// let age = 23
+// let isStudent = true
+// let address = "Dhaka , Bangladesh"
+// console.log(name)
+
+// let user1 = {
+//   name: "john",
+//   age: 23,
+//   isStudent: true,
+//   address: "Dhaka, Bangladesh",
+// };
+// console.log(user1);
+
+//delete
+// console.log(delete user1.age)
+// console.log(user1)
+
+// set
+// user1.ismarrid = false
+// console.log(user1)
+
+// let user2 = {
+//   name: "Mac",
+//   age: 53,
+//   "like video games": true,
+// };
+
+// get
+// console.log(user2["like video games"]);
+// console.log(user2["age"]);
+
+// set
+// user2["isMarrid"] = false;
+// console.log(user2);
+
+// delete
+// delete user2["like video games"];
+// console.log(user2);
