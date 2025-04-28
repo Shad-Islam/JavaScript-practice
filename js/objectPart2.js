18a7eeab33aa9e1a7c1845abcabd69726df2ff76
Shad-islam <m.shadislam01@gmail.com>
2025-04-28 22:07:41 +0600

    Class 5

---
+++ js/objectPart2.js
@@ -0,0 +1,35 @@
+// Computed properties
+
+// let fruit = prompt("Which fruit?", "apple");
+// let bag = {
+//   [fruit]: 5,
+// };
+// alert(bag.apple)
+
+// let fruit = "apple";
+// let bag = {
+//   [fruit + "Str"]: 15,
+// };
+// alert(bag.appleStr);
+
+
+
+// Property value shorthand
+
+// let name = "Shad";
+// let id = 319;
+// let obj = {
+//   name, //same ase name:name
+//   id,  //same ase id:id
+// };
+// console.log(obj.name);
+// console.log(obj.id);
+
+
+// Property names limitations
+// let obj = {
+//     for: 1,
+//     let: 2,
+//   };
+// console.log(obj.for)
+// console.log(obj.let)
