8a5712c2eb813222fa185a4da37c73b1489737ad
Shad-islam <m.shadislam01@gmail.com>
2025-05-08 03:04:28 +0600

    Day 7: Object copy by reference

--- js/objectCopy.js
+++ js/objectCopy.js
@@ -1,10 +1,28 @@
-let user = {
-  name: "Shad",
-};
+// variable memory alocation
+// let name = "Ridul"
+// console.log(name) //Ridul
+// let name2 = name
+// console.log(name2) //Ridul
+// name = "Shad"
+// console.log(name2) //Ridul Shad
+// console.log(name)
 
-let user2 = user;
 
-// console.log(user2);
-user2.name = "Abrar"
+// object copy by ref
 
-console.log(user.name)
+// let obj = {
+//   name: "John",
+//   age: 20,
+//   isStudent: true,     
+// };
+
+// let obj2 =  obj
+
+
+// console.log(obj.name) //john
+
+// obj.name = 'Alice'
+
+// console.log(obj.name) //Alice
+
+// console.log(obj2.name) // Alice
