3efe23f590350dac6bcd5c117a4c0b3404b68520
Shad-islam <m.shadislam01@gmail.com>
2025-05-08 20:57:41 +0600

    Day 8 Cloning object

--- js/objectCopy.js
+++ js/objectCopy.js
@@ -7,22 +7,41 @@
 // console.log(name2) //Ridul Shad
 // console.log(name)
 
-
 // object copy by ref
 
 // let obj = {
 //   name: "John",
 //   age: 20,
-//   isStudent: true,     
+//   isStudent: true,
 // };
 
 // let obj2 =  obj
 
-
 // console.log(obj.name) //john
-
 // obj.name = 'Alice'
-
 // console.log(obj.name) //Alice
-
 // console.log(obj2.name) // Alice
+
+// object clone
+
+// let obj1 = {
+//   name: "Abrar",
+//   id: 1,
+//   isStudent: true,
+//   location: "Dhaka",
+// };
+
+// // console.log(obj1.id);
+
+// let obj2 = {};
+
+// for (key in obj1) {
+//     obj2[key] = obj1[key]
+// }
+
+// // console.log(obj2)
+
+// obj2.name = "John"
+// obj2.id = 2
+// console.log(obj2)
+// console.log(obj1)
