9db8c053b1fcf1cfafd50b81002ee5f9a920d780
Shad-islam <m.shadislam01@gmail.com>
2025-05-14 23:51:55 +0600

    function declaretion

---
+++ js/function.js
@@ -0,0 +1,56 @@
+// let a = 10;
+// let b = 20;
+// // function declare 
+// function showText() {
+//   console.log(a + b);
+//   console.log("Hello js");
+// }
+// // function call
+// showText();
+
+// function addition( a, b){
+//     console.log(a+b)
+// }
+
+// addition(10,20)
+
+// let text = prompt("What is ur name?");
+// function greetings(text) {
+//   console.log(`${text}`);
+// }
+// greetings(text)
+
+// let name = "Shad"
+// let line = `hello ${name},how are u?`
+// console.log(line)
+
+// local veriable and outer veriable
+
+// let name = "Shad";
+// function showText() {
+//   name = "Abrar";
+//   console.log(name);
+// }
+
+// showText();
+
+// outer variables
+// let name = "Shad";
+// function showText() {
+//   name = "Abrar";
+//   console.log(name);
+// }
+// console.log(name)//shad
+// showText();//abrar
+// console.log(name)//abrar
+
+
+//local variable
+// function showText() {
+//  let  name = "Abrar";
+//   console.log(name);
+// }
+// showText()
+// console.log(name)
+
+
