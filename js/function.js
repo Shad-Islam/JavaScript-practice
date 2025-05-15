26883d955f1b4febc5f5887866d3e2a1242d5913
Shad-islam <m.shadislam01@gmail.com>
2025-05-15 21:09:23 +0600

    Function

--- js/function.js
+++ js/function.js
@@ -1,6 +1,6 @@
 // let a = 10;
 // let b = 20;
-// // function declare 
+// // function declare
 // function showText() {
 //   console.log(a + b);
 //   console.log("Hello js");
@@ -44,7 +44,6 @@
 // showText();//abrar
 // console.log(name)//abrar
 
-
 //local variable
 // function showText() {
 //  let  name = "Abrar";
@@ -52,5 +51,68 @@
 // }
 // showText()
 // console.log(name)
+
+// default parameter//
+// function showText(name, text = "Hello") {
+//   console.log(name + " : " + text);
+// }
+// showText("Shad");
+
+// function showText(name, text) {
+//   if (text === undefined) {
+//     text = "Abrar kisu dai nai";
+//   }
+//   console.log(name + " : " + text);
+// }
+// showText("Shad", "Hello js");
+
+// function showText(name, text) {
+//   text = text || "No text given";
+//   console.log(name + " : " + text);
+// }
+// showText("Shad");
+
+// function showText(name, text) {
+//   console.log(name + " : " + text);
+// }
+// showText();
+
+// return //
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(90,2));
+
+// function doNothing() {}
+// console.log(doNothing() === undefined);
+
+// function expression
+
+// function sayHi() {
+//   console.log("Hello");
+// }
+
+// let sayHi = function () {
+//   console.log("hello");
+// };
+// let func = sayHi
+// func()
+
+
+// function callback
+// function ask(question, yes, no) {
+//     let result = confirm(question);
+//   if (result) yes();
+//   else no();
+// }
+
+// function showOk() {
+//   console.log("Yes, I agree");
+// }
+// function showCancel() {
+//   console.log("Cancel the execution");
+// }
+
+// ask("Do you agree?", showOk, showCancel);
 
 
