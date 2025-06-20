20a5f7174a03b446098cc2d1eab32db8fe268f31
Shad-islam <m.shadislam01@gmail.com>
2025-06-20 15:59:51 +0600

    Add variables

---
+++ js/variable.js
@@ -0,0 +1,20 @@
+let name = "Shad";
+const name2 = "Abrar";
+
+name = "rohim";
+// name2 = "korim";
+
+// var name3 = "Ridul";
+
+console.log(name, name2);
+
+const obj = {
+  name: "a",
+  age: 20,
+};
+
+console.log(obj);
+
+obj.name = "Shad";
+
+console.log(obj);
