5a55a74104a6025fc069f2bda268d9c65d6ae009
Shad-islam <m.shadislam01@gmail.com>
2025-06-29 20:46:54 +0600

    Add array methods

---
+++ js/array_methods.js
@@ -0,0 +1,72 @@
+// let arr = [200, 300, 400, 500];
+// console.log(arr);
+
+// add items to the end
+// arr.push(600);
+// console.log(arr);
+
+// extracts an item from the end
+// arr.pop();
+// console.log(arr);
+
+// add items to the beginning
+// arr.unshift(100);
+// console.log(arr);
+
+// extracts an item from the beginning
+// arr.shift();
+// console.log(arr);
+
+// delete an element from the array
+// delete arr[1];
+// console.log(arr);
+// console.log(arr.length);
+// exp
+// index: [0,1,2,3,4]
+// value: [1, ,3,4,5]
+
+// splice
+
+// let arr2 = ["I", "love", "code"];
+// arr2.splice(1, 1);
+// console.log(arr2);
+// console.log(arr2.length);
+
+// let arr3 = ["I", "study", "js", "right", "now"];
+// arr3.splice(2, 1);
+// console.log(arr3);
+
+// remove 3 elements and replace them
+// arr3.splice(0, 3, "Hello", "world");
+// console.log(arr3);
+
+// insert element without any removals
+// arr3.splice(3, 0, "in", "Bangladesh");
+// console.log(arr3);
+
+// let arr4 = ["I", "love", "mac", "very", "much"];
+// let removeElemets = arr4.splice(0, 3);
+// console.log(removeElemets);
+
+// let arr5 = ['a','b','c','d','e']
+// console.log(arr5.slice(1,3))
+// console.log(arr5)
+
+//concat
+// let firstName = ["S", "h", "a", "d"];
+// let lastName = ["I", "s", "l", "a", "m"];
+
+// let fullName = firstName.concat(lastName);
+// console.log(fullName);
+
+// let names = ["Shad", "Ridul", "Abrar"];
+// function addId(arr) {
+//   arr.forEach((item, index) => {
+//     console.log(`${item}'s id ${index}`);
+//   });
+// }
+// addId(names);
+
+
+
+
