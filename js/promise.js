0ad9010c09a151f4e6aaa897a8014d9f6f09871d
Shad-islam <m.shadislam01@gmail.com>
2025-09-20 01:52:06 +0600

    Add promise

---
+++ js/promise.js
@@ -0,0 +1,27 @@
+console.log("Promise Example");
+
+const myPromise = new Promise((resolve, reject) => {
+  const success = false; // Change to false to test rejection
+  setTimeout(() => {
+    if (success) {
+      resolve("Promise resolved successfully!");
+    } else {
+      reject("Promise rejected!");
+    }
+  }, 2000);
+});
+
+
+
+
+
+myPromise
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
+    console.log("Promise has been settled (either resolved or rejected).");
+  });   
