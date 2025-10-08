bac8fecf3f576ee5d8c6d8734c6af773c2ad4124
Shad-islam <m.shadislam01@gmail.com>
2025-10-08 17:55:56 +0600

    Add callback hell

--- js/async.js
+++ js/async.js
@@ -1,11 +1,53 @@
-const doSomething = () => {
+// const doSomething = () => {
+//   console.log("Doing something...");
+//   let currentTime = new Date().getTime();
+//   while (currentTime + 5000 >= new Date().getTime()) {}
+//   console.log("Done something after 5 seconds");
+// };
+
+// console.log("Start");
+// doSomething();
+// console.log("End");
+
+// asynchronous //
+
+// const doSomething = () => {
+//   console.log("Doing something...");
+
+//   setTimeout(() => {
+//     console.log("Done something after 5 seconds");
+//   }, 5000);
+// };
+
+// console.log("Start");
+// doSomething();
+// console.log("End");
+
+// callback hell //
+
+const startWork = (user, callback) => {
+  console.log("Start");
+  callback(user);
+};
+
+const doSomething = (user, callback) => {
   console.log("Doing something...");
 
   setTimeout(() => {
     console.log("Done something after 5 seconds");
+    console.log("End");
+    callback(user);
   }, 5000);
 };
 
-console.log("Start");
-doSomething();
-console.log("End");
+const completeWork = (user) => {
+  console.log(`All work completed for ${user}`);
+};
+
+startWork("Abrar", (user) => {
+  doSomething(user, (user) => {
+    completeWork(user);
+  });
+});
+
+console.log("Hello world");
