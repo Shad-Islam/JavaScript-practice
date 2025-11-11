68a4830d1971c5c0a2190e8c8b300dfb923aaa3c
Shad-islam <m.shadislam01@gmail.com>
2025-11-11 18:31:22 +0600

    Add tail and non tail recursive faction

--- js/recursive.js
+++ js/recursive.js
@@ -16,33 +16,49 @@
 // }
 // printNumber(5);
 
-let arr = [2, 4, 6, 8, 10];
+// let arr = [2, 4, 6, 8, 10];
 
-const person = {
-  name: "Shad",
-  info: {
-    age: 26,
-    address: {
-      city: "Dhaka",
-      homeTown: "Khulna",
-    },
-  },
-};
+// const person = {
+//   name: "Shad",
+//   info: {
+//     age: 26,
+//     address: {
+//       city: "Dhaka",
+//       homeTown: "Khulna",
+//     },
+//   },
+// };
 
-function printObj(obj) {
-  for (let key in obj) {
-    if (typeof obj[key] === "object") {
-      printObj(obj[key]);//recursive case 
-    } else {
-      console.log(`${key}: ${obj[key]}`);
-    }
-  }
-}
+// function printObj(obj) {
+//   for (let key in obj) {
+//     if (typeof obj[key] === "object") {
+//       printObj(obj[key]); //recursive case
+//     } else {
+//       console.log(`${key}: ${obj[key]}`);
+//     }
+//   }
+// }
 
-printObj(person)
+// printObj(person);
 
 // output :
 // name : Shad
 // age : 26
 // city : Dhaka
 // homeTown : Khulna
+
+// tail recursive
+function factorialTail(n, acc) {
+  if (n == 0) return acc; // base case
+  return factorialTail(n - 1, n * acc); //recursive case
+}
+
+console.log(factorialTail(5, 1));
+
+// non-tail recursive
+function factorialTail(n) {
+  if (n == 0) return 1; // base case
+  return n * factorialTail(n-1); //recursive case
+}
+
+console.log(factorialTail(5));
