c0cb72c6ba9a924385aafe7ca1ae877d5a3315eb
Shad-islam <m.shadislam01@gmail.com>
2025-11-11 11:37:50 +0600

    Add recursive practice code

--- js/recursive.js
+++ js/recursive.js
@@ -1,12 +1,48 @@
-function sum(n) {
-  if (n === 1) {
-    return 1;
-  }
+// function sum(n) {
+//   if (n === 1) {  //base case
+//     return 1;
+//   }
 
-  return n + sum(n - 1);
+//   return n + sum(n - 1);  //recursive case
+// }
+
+// let n = 5;
+// console.log(sum(n));
+
+// function printNumber(number) {
+//   if (number === 0) return;
+//   printNumber(number - 1);
+//   console.log(number);
+// }
+// printNumber(5);
+
+let arr = [2, 4, 6, 8, 10];
+
+const person = {
+  name: "Shad",
+  info: {
+    age: 26,
+    address: {
+      city: "Dhaka",
+      homeTown: "Khulna",
+    },
+  },
+};
+
+function printObj(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      printObj(obj[key]);//recursive case 
+    } else {
+      console.log(`${key}: ${obj[key]}`);
+    }
+  }
 }
 
-let n = 5;
-console.log(sum(n));
+printObj(person)
 
-sum(n - 1);
+// output :
+// name : Shad
+// age : 26
+// city : Dhaka
+// homeTown : Khulna
